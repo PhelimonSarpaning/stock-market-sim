ede9d30c2066daeb5ea6d4a89ac9129cda2d6372
murtazasmart <murtazasmart@hotmail.com>
2018-05-29 07:03:27 +0530

    Config files

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,14 +1,22 @@
 import { Container } from "inversify";
 import "reflect-metadata";
 
-import { MarketTrendService, ProbabilityService, SectorTrendService } from "./services/";
+import { Game, Round, Stock } from "./models";
+import { GameService, MarketEventsService, MarketTrendsService, ProbabilityService, RandomTrendsService,
+  SectorTrendsService } from "./services/";
 import { Logger } from "./util/Logger";
 
 const GlobalDI = new Container();
 
 GlobalDI.bind<Logger>("Logger").to(Logger).inSingletonScope();
-GlobalDI.bind<MarketTrendService>("MarketTrendService").to(MarketTrendService);
+GlobalDI.bind<MarketTrendsService>("MarketTrendsService").to(MarketTrendsService);
 GlobalDI.bind<ProbabilityService>("ProbabilityService").to(ProbabilityService);
-GlobalDI.bind<SectorTrendService>("SectorTrendService").to(SectorTrendService);
+GlobalDI.bind<SectorTrendsService>("SectorTrendsService").to(SectorTrendsService);
+GlobalDI.bind<GameService>("GameService").to(GameService);
+GlobalDI.bind<MarketEventsService>("MarketEventsService").to(MarketEventsService);
+
+GlobalDI.bind<Game>("Game").to(Game);
+GlobalDI.bind<Round>("Round").to(Round);
+GlobalDI.bind<Stock>("Stock").to(Stock);
 
 export { GlobalDI };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,6 @@
-export * from "./MarketTrendService";
+export * from "./MarketTrendsService";
 export * from "./ProbabilityService";
 export * from "./SectorTrendsService";
+export * from "./MarketEventsService";
+export * from "./RandomTrendsService";
+export * from "./GameService";
